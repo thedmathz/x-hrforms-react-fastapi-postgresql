79171bc3041148aec320e8dbef7389c11fee0805
Rogincel Demata <roginceldemata@gmail.com>
2026-01-12 00:03:16 +0800

    feature/positions: finished crud

--- server/app/api/v1/endpoints/positions.py
+++ server/app/api/v1/endpoints/positions.py
@@ -1,31 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.session import get_db
+from app.services.position_service import PositionService
 
-router = APIRouter()
+from app.schemas.position import PositionInsert, PositionUpdate
+
+router  = APIRouter()
+service = PositionService()
 
 @router.get("/")
-async def index():
-    return {"key": "element"}
-
-@router.get("/add")
-async def add():
-    return {"key": "element"}
+async def index(db: AsyncSession = Depends(get_db)):
+    return await service.index(db)
 
 @router.post("/")
-async def insert():
-    return {"key": "element"}
+async def insert(form_request: PositionInsert, db: AsyncSession = Depends(get_db)):
+    return await service.insert(db, form_request) 
 
 @router.get("/{id}")
-async def view(id: str):
-    return {"key": "element"}
-
-@router.get("/{id}/edit")
-async def edit(id: str):
-    return {"key": "element"}
+async def view(id: str, db: AsyncSession = Depends(get_db)):
+    return await service.view(db, id) 
 
 @router.put("/{id}")
-async def update(id: str):
-    return {"key": "element"}
+async def update(id: str, form_request: PositionUpdate, db: AsyncSession = Depends(get_db)):
+    return await service.update(db, id, form_request)
 
 @router.delete("/{id}")
-async def delete(id: str):
-    return {"key": "element"}
+async def delete(id: str, db: AsyncSession = Depends(get_db)):
+    return await service.delete(db, id)

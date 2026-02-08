f1fb57e4e632d2d0661e3c9eaf678a994e8d6923
Rogincel Demata <roginceldemata@gmail.com>
2026-02-08 13:42:52 +0800

    feature/register: pip -> poetry

--- server/app/api/v1/api.py
+++ server/app/api/v1/api.py
@@ -15,10 +15,10 @@ from app.api.v1.endpoints import (
     user_types,
 )
 
-api_router = APIRouter()
+api_router = APIRouter()    
 
 api_router.include_router(starter.router, prefix="/starter", tags=["Starter"])
-# api_router.include_router(register.router, prefix="/register", tags=["Register"])
+api_router.include_router(register.router, prefix="/register", tags=["Register"])
 api_router.include_router(forgot_password.router, prefix="/forgot-password", tags=["Forgot Password"])
 api_router.include_router(authentication.router, prefix="/authentication", tags=["Authentication"])
 api_router.include_router(my_profile.router, prefix="/my-profile", tags=["My Profile"])
@@ -28,5 +28,5 @@ api_router.include_router(change_password.router, prefix="/change-password", tag
 # api_router.include_router(for_approvals.router, prefix="/for-approvals", tags=["For Approvals"])
 api_router.include_router(offices.router, prefix="/offices", tags=["Offices"])
 api_router.include_router(positions.router, prefix="/positions", tags=["Positions"])
-api_router.include_router(users.router, prefix="/users", tags=["Users"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"]) 
 api_router.include_router(user_types.router, prefix="/user-types", tags=["User Types"])
